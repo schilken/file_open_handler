00c74f62692481ed3f00cb06bd1e3a21b9a644a8
Alfred Schilken <alfred@schilken.de>
2022-08-20 20:49:49 +0200

    call handleFileOpen from AppDelegate

--- example/macos/Runner/AppDelegate.swift
+++ example/macos/Runner/AppDelegate.swift
@@ -1,9 +1,17 @@
 import Cocoa
 import FlutterMacOS
+import file_open_handler
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
+
+       override func application(_ sender: NSApplication, openFile filename: String) -> Bool {
+         FileOpenHandlerPlugin.instance.handleFileOpen(pathname: filename);
+         // Return true if your app opened the file successfully, false otherwise
+         return true
+     }
+
 }
--- macos/Classes/FileOpenHandlerPlugin.swift
+++ macos/Classes/FileOpenHandlerPlugin.swift
@@ -2,18 +2,39 @@ import Cocoa
 import FlutterMacOS
 
 public class FileOpenHandlerPlugin: NSObject, FlutterPlugin {
+  private static var _instance: FileOpenHandlerPlugin?
+  private var _initialFilepath: String?
+
+      public static var instance: FileOpenHandlerPlugin {
+      get {
+        return _instance!
+      }
+    }
+
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "file_open_handler", binaryMessenger: registrar.messenger)
     let instance = FileOpenHandlerPlugin()
+        _instance = instance
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
     case "openedWithFile":
-      result("ttt.md")
+      result(_initialFilepath ?? "no file")
     default:
       result(FlutterMethodNotImplemented)
     }
   }
+
+      @objc
+    public func handleFileOpen(pathname: String) {
+//        print("openning file \(pathname)")
+        // let args: NSDictionary = [
+        //     "filepath": pathname,
+        // ]
+        _initialFilepath = pathname
+//        channel.invokeMethod("onFileOpen", arguments: args, result: nil)
+    }
 }
